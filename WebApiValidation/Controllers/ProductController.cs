a10944d202798abdf962b2ae5e1d6d0f4854861d
kevin <kevin@puhanda.com>
2018-10-15 14:13:54 +0800

    validatable object

---
+++ WebApiValidation/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiValidation.Filters;
+using WebApiValidation.Models;
+
+namespace WebApiValidation.Controllers
+{
+    public class ProductController : ApiController
+    {
+        [ParamsFilter]
+        public IHttpActionResult Post(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok();
+            }else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
